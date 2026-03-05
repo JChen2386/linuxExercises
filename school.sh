50082177fa897ba8bfc2ce8bbb17f569809cbf09
Jiahao Chen <JChen2386@wisc.edu>
2026-03-05 10:15:35 -0600

    Finish School.sh

---
+++ school.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d',' -f7 | {
+    sum=0
+    n=0
+    while read value; do
+	sum=$(($sum + $value))
+	n=$(($n+1))
+    done
+    avg=$(($sum/$n))
+    echo $avg
+    }
