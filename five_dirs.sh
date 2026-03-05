2cbded1154381a0685005c6b6e5a94d98c83b3e8
Jiahao Chen <JChen2386@wisc.edu>
2026-03-05 10:53:52 -0600

    done

---
+++ five_dirs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir five
+for d in {1..5}; do
+    mkdir "five/dir$d"
+    for k in {1..4}; do
+	file=five/dir$d/file$k
+	for i in $(seq 1 $k); do
+	    echo $k >> $file
+	done
+    done
+done
