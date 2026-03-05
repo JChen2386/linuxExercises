798022bac523f3e0ae001f3d6273c00ff3274f59
Sambhav Mahajan <smahajan28@wisc.edu>
2026-03-05 10:38:48 -0600

    Completed mean.sh and added test data

--- mean.sh
+++ mean.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "usage: $0 <column> [file.csv]" 1>&2
+    exit 1
+fi
+
+column=$1
+
+file=${2:-/dev/stdin}
+cut -d, -f"$column" "$file" | tail -n +2 | {
+    sum=0
+    count=0
+    while read val; do
+sum=$(echo "$sum + $val" | bc)
+        count=$((count + 1))
+    done
+if [ $count -gt 0 ]; then
+        echo "scale=2; $sum / $count" | bc
+    fi
+}
+
+
